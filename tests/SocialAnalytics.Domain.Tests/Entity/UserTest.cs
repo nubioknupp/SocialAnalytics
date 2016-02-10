02405f67217eef025ec7b6d747b8fd46ebb01c7c
Núbio Knupp <nubioknupp@gmail.com>
2016-02-10 10:02:33 -0200

    Tests e AutoMapper
    Add project of Tests.
    Add AutoMapper in Application.

---
+++ tests/SocialAnalytics.Domain.Tests/Entity/UserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SocialAnalytics.Domain.Tests.Entity
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
